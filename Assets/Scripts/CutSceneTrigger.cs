60224447db50f9d81e9c295d66190ef7dd2b8d13
seungdo1234 <wltmdeh12@naver.com>
2023-05-25 01:10:13 +0900

    23.05.24

--- Assets/Scripts/CutSceneTrigger.cs
+++ Assets/Scripts/CutSceneTrigger.cs
@@ -14,9 +14,13 @@ public class CutSceneTrigger : MonoBehaviour
     private EnemySpawner enemySpawner;
     [SerializeField]
     private GameManager gameManager;
+    [SerializeField]
+    private int stageNum;
+    [SerializeField]
+    private GameObject transparentWall; // 컷신이 끝나도 전으로 못가게 하기위한 투명벽
 
     private int triggerNum = 0; // 트리거 레이어 위치
-    private int sceneNum = 1; // 불러올 씬 넘어
+    private int sceneNum = 1; // 불러올 씬 넘버
     private bool cutScenePlaying; // 컷신 플레이 중
     private RaycastHit2D rayHit;
 
@@ -32,10 +36,18 @@ public class CutSceneTrigger : MonoBehaviour
         cutScenePlaying = true;
         gameManager.PlayCutScene(sceneNumber, delay);
         yield return new WaitForSeconds(delay);
-        enemySpawner.SpanwnEnemy();
-        dropLayer.SpawnPointChange();
+        if(stageNum == 1)
+        {
+            enemySpawner.SpanwnEnemy();
+            dropLayer.SpawnPointChange();
+        }
+        if(stageNum == 2)
+        {
+            transparentWall.SetActive(true);
+        }
         triggerNum++;
         sceneNum++;
+        isTrigger = false;
         cutScenePlaying = false;
     }
     private void Update()
@@ -43,7 +55,7 @@ public class CutSceneTrigger : MonoBehaviour
         if (isTrigger)
         {
             Debug.DrawRay(layerPoint[triggerNum].position, Vector3.up * 10.0f, new Color(0, 1, 0));
-            rayHit = Physics2D.Raycast(layerPoint[triggerNum].position, Vector3.up, 10f, LayerMask.GetMask("Player")); // 위로 레이어 발사
+            rayHit = Physics2D.Raycast(layerPoint[triggerNum].position, Vector3.up, 10f, LayerMask.GetMask("Player")); // 위로 레이어 발사 
             if (rayHit.collider != null)
             {
                 // rayHit.transform.position = spawnPoint.position;
@@ -51,7 +63,15 @@ public class CutSceneTrigger : MonoBehaviour
                 {
                     rayHit.collider.GetComponent<Animator>().SetBool("isJumpping", false); // 점프 중인 경우 점프 애니메이션 해제
                     rayHit.collider.GetComponent<Animator>().SetBool("IsJumpDown", false); // 점프 중인 경우 점프 애니메이션 해제
-                    StartCoroutine(PlayCutScene(sceneNum, 7.5f));
+                    rayHit.collider.GetComponent<Animator>().SetBool("isRunning", false); // 달리기 중인 경우 달리기 애니메이션 해제
+                    if (stageNum == 1)
+                    {
+                        StartCoroutine(PlayCutScene(sceneNum, 7.5f));   
+                    }
+                    else if(stageNum == 2)
+                    {
+                        StartCoroutine(PlayCutScene(0, 21f));
+                    }
                 }
 
             }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,23 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
+    private int stageNum;
+    [SerializeField]
     private CutScene[] cutScene;
     [SerializeField]
     private Player player;
+    [SerializeField]
+    private Slider screenSlider; // 컷신 전 후 나오는 화면 가리개
+    [SerializeField]
+    private float sliderSpeed  = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
-        PlayCutScene(0, 7f);
+        if(stageNum == 1)
+        {
+            PlayCutScene(0, 7f);
+        }
     }
-
+    
+    private IEnumerator ScreenSlider(int sceneNumber, float delay)
+    {
+        player.isCutScenePlaying = true;
+        bool end = false;
+        screenSlider.value = 0;
+        while(true) // 컷신 전 슬라이드 작동
+        {
+            if (!end)
+            {
+                screenSlider.value += Time.deltaTime;
+            }
+            else if(end)
+            {
+                screenSlider.value -= Time.deltaTime;
+            }
+            if (screenSlider.value >= 1)
+            {
+                end = true;
+            }
+            if(end && screenSlider.value == 0)
+            {
+                break;
+            }
+            yield return null;
+        }
+        cutScene[sceneNumber].PlayCutScene(delay); // 컷신 플레이
+        yield return new WaitForSeconds(delay); // 컷신 딜레이 만큼 멈추기
+        end = false;
+        while (true) // 컷신 후 슬라이드 작동
+        {
+            if (!end)
+            {
+                screenSlider.value += Time.deltaTime;
+            }
+            else if (end)
+            {
+                screenSlider.value -= Time.deltaTime;
+            }
+            if (screenSlider.value >= 1)
+            {
+                end = true;
+            }
+            if (end && screenSlider.value == 0)
+            {
+                break;
+            }
+            yield return null;
+        }
+        player.isCutScenePlaying = false;
+    }
     public void PlayCutScene(int sceneNumber, float delay)
     {
-        cutScene[sceneNumber].PlayCutScene(delay);
-        StartCoroutine(CutScenePlaying(delay));
+        StartCoroutine(ScreenSlider(sceneNumber, delay));
     }
     private IEnumerator CutScenePlaying(float delay)
     {
